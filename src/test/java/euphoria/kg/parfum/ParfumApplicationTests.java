fe7bcd80150ace76237d133dd262e1bd0f425bf4
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-04-30 16:27:32 +0600

    Connection to MySQl

---
+++ src/test/java/euphoria/kg/parfum/ParfumApplicationTests.java
@@ -0,0 +1,13 @@
+package euphoria.kg.parfum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParfumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
