ee69a187f465e2eb8663b36561d1b329ed7e82bc
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-05-11 16:18:39 +0600

    Lesson68.Create exception for Customer

---
+++ src/main/java/euphoria/kg/parfum/exception/CustomerAlreadyRegisteredException.java
@@ -0,0 +1,8 @@
+package euphoria.kg.parfum.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class CustomerAlreadyRegisteredException extends RuntimeException { }
