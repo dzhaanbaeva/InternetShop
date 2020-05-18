a9de7ac5c50b750b2adfa88f727c250adbe89899
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-05-18 13:25:11 +0600

    View cart

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,38 @@ window.addEventListener('load', function () {
 
     });
 
+
+
+
 });
+
+function addProduct(id){
+    console.log(id, "HERE")
+    // let name1 = "_csrf"
+    // let name = document.getElementsByName("_csrf_header")[0].getAttribute("content")
+    // let value = document.getElementsByName("_csrf_token")[0].getAttribute("content")
+    // let data = new FormData();
+    // data.append("id",id);
+    // data.append(name1,value);
+    //
+    // let product = document.getElementById("product"+id)
+    // if(product.classList.contains("fa-cart-plus")){
+    //     product.classList.remove("fa-cart-plus")
+    //     product.classList.add("fa-cart-arrow-down")
+    // } else {
+    //     product.classList.remove("fa-cart-arrow-down")
+    //     product.classList.add("fa-cart-plus")
+    // }
+    //
+    // fetch("http://localhost:8000/products/addInBasket/", {
+    //     method: 'POST',
+    //     body: data
+    // }).then(r => r.json());
+    // console.log(id);
+    // console.log(name1);
+    // console.log(value);
+
+}
 
 const getCurrentPage = () => {
     const loc = (typeof window.location !== 'string') ? window.location.search : window.location;
