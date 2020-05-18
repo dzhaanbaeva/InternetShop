fa06ca05413a73339399d368ba83c13761f325c3
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-05-18 13:24:06 +0600

    add to MYSQL

--- src/main/resources/db/migration/V1001__initial_database_setup.sql
+++
@@ -1,9 +0,0 @@
-use `parfums`;
-
-CREATE TABLE `brands` (
-    `id` INT auto_increment NOT NULL,
-    `brand` varchar(128) NOT NULL,
-    PRIMARY KEY (`id`)
-);
-
-
--- src/main/resources/db/migration/V1002__add_categories_table.sql
+++
@@ -1,8 +0,0 @@
-use `parfums`;
-
-create table `categories` (
-    `id` INT auto_increment NOT NULL,
-    `category` varchar(128) NOT NULL,
-     PRIMARY KEY (`id`)
-    );
-
